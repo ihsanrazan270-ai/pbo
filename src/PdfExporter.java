fa8dc83a46091182cbfc2ea1a047e2c5855e620b
kal2301 <muhammadhaikalhizriyanachmyzad@gmail.com>
2026-06-08 10:54:54 +0700

    haikal masuk

--- src/PdfExporter.java
+++ src/PdfExporter.java
@@ -1,8 +1,3 @@
-import java.io.FileOutputStream;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
-
 public class PdfExporter {
 
     public static void exportPDF() {
